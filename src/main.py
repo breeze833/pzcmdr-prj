a07af011c4194db051801d5b096dc6acf7b4d33e
Feng-Cheng Chang <breeze833@gmail.com>
2018-11-15 04:40:59 +0000

    Restructure the main program.

--- src/main.py
+++ src/main.py
@@ -14,89 +14,94 @@ from cmdr import JPEG2Image, Image2Gray, QRCodeScanner
 from cmdr import RGB_LED
 from cmdr import CmdExec
 
-class SysSigHandler:
-    def __init__(self, iddb_file, cmds_file):
+class App:
+    def setup_signal_handlers(self):
         """
         Initialize the signal handlers.
-
-        Arguments:
-        iddb_file -- The path to the ID-to-Cmd lookup table.
-        cmds_file -- The path to the Cmd-to-ExecCode lookup table.
         """
-        self.iddb_file = iddb_file
-        self.cmds_file = cmds_file
         loop = asyncio.get_event_loop()
         loop.add_signal_handler(signal.SIGTERM, self.exit_gracefully)
         loop.add_signal_handler(signal.SIGINT, self.exit_gracefully)
         loop.add_signal_handler(signal.SIGUSR1, self.reload)
         loop.add_signal_handler(signal.SIGUSR2, self.toggle_leak_uid)
         loop.add_signal_handler(signal.SIGCHLD, self.child_exit)
+
     def exit_gracefully(self):
         logger.debug('cancelling tasks...')
         [task.cancel() for task in asyncio.Task.all_tasks()]
         asyncio.get_event_loop().stop()
+
     def reload(self):
         try:
-            lookup_tables = load_lookup_tables(self.iddb_file, self.cmds_file)
+            lookup_tables = self.load_lookup_tables()
             dispatcher.send(signal="reload", sender=dispatcher.Any, commands=lookup_tables['commands'], uids=lookup_tables['uids'], qrcodes=lookup_tables['qrcodes'])
         except:
             dispatcher.send(signal="got_error")
+
     def toggle_leak_uid(self):
         dispatcher.send("toggle_leak_uid")
+        
     def child_exit(self):
         logger.debug('child process exit')
         os.waitpid(0, 0)
 
-def load_lookup_tables(iddb_file, cmds_file):
-    exec_g = {}
-    exec_l = {}
-    try:
-        exec(open(iddb_file).read(), exec_g, exec_l)
-        exec(open(cmds_file).read(), exec_g, exec_l)
-    except:
-        dispatcher.send(signal='got_error')
-    finally:
-        return exec_l
+    def load_lookup_tables(self):
+        """
+        Load lookup tables from files.
+        """
+        exec_g = {}
+        exec_l = {}
+        try:
+            exec(open(self.iddb_file).read(), exec_g, exec_l)
+            exec(open(self.cmds_file).read(), exec_g, exec_l)
+        except:
+            dispatcher.send(signal='got_error')
+        finally:
+            return exec_l
 
-def main(log_level=logging.INFO):
-    logging.basicConfig(level=log_level)
+    def setup_processing_components(self):
+        lookup_tables = self.load_lookup_tables()
+        self.rfid_reader = RC522()
+        self.serial_camera = VC0706()
+        self.uid2cmd = UID2Cmd(lookup_tables['uids'])
+        self.qrcode2cmd = QRCode2Cmd(lookup_tables['qrcodes'])
+        self.uid_logger = UIDLogger()
+        self.jpeg2image = JPEG2Image()
+        self.image2gray = Image2Gray()
+        self.qrcode_scanner = QRCodeScanner()
+        self.rgb_led = RGB_LED()
+        self.cmdexec = CmdExec(lookup_tables['commands'])
 
-    iddb_file = './config/iddb.py'
-    cmds_file = './config/cmds.py'
+    def __init__(self, log_level=logging.INFO):
+        logging.basicConfig(level=log_level)
 
-    lookup_tables = None
-    try:
-        iddb_file = sys.argv[1]
-        cmds_file = sys.argv[2]
-    except:
-        pass
-    finally:
-        lookup_tables = load_lookup_tables(iddb_file, cmds_file)
-        logger.info('ID file is {idfile} and CMD file is {cmdfile}'.format(idfile=iddb_file, cmdfile=cmds_file))
+        self.iddb_file = './config/iddb.py'
+        self.cmds_file = './config/cmds.py'
 
-    loop = asyncio.get_event_loop()
+        lookup_tables = None
+        try:
+            self.iddb_file = sys.argv[1]
+            self.cmds_file = sys.argv[2]
+        except:
+            pass
+        finally:
+            logger.info('ID file is {idfile} and CMD file is {cmdfile}'.format(idfile=self.iddb_file, cmdfile=self.cmds_file))
 
-    sig_handlers = SysSigHandler(iddb_file, cmds_file)
+        self.setup_signal_handlers()
+        self.setup_processing_components()
+        
+    def run(self):
+        dispatcher.send("start")
 
-    rfid_reader = RC522()
-    serial_camera = VC0706()
-    uid2cmd = UID2Cmd(lookup_tables['uids'])
-    qrcode2cmd = QRCode2Cmd(lookup_tables['qrcodes'])
-    uid_logger = UIDLogger()
-    jpeg2image = JPEG2Image()
-    image2gray = Image2Gray()
-    qrcode_scanner = QRCodeScanner()
-    rgb_led = RGB_LED()
-    cmdexec = CmdExec(lookup_tables['commands'])
-
-    dispatcher.send("start")
-
-    try:
-        loop.run_forever()
-    finally:
-        logger.debug('waiting for all tasks done...')
-        loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks(), return_exceptions=True))
-        loop.close()
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_forever()
+        finally:
+            logger.debug('waiting for all tasks done...')
+            loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks(), return_exceptions=True))
+            loop.close()
 
 if __name__=='__main__':
-    main(log_level=logging.__dict__[os.getenv('PZCMDR_LOGLEVEL','INFO')])
+    app = App(log_level=logging.__dict__[os.getenv('PZCMDR_LOGLEVEL','INFO')])
+    app.run()
+
